404bc66e33b00097f9a3aca4182ba80c53bc3dc1
Manteiga <rafael.manteiga@hotmail.com>
2023-07-21 14:53:32 -0300

    Add files via upload

--- blackjack.py
+++ blackjack.py
@@ -9,19 +9,12 @@ try:
         pergunta = int(input("\n1 - Puxar mais uma carta\n2 - Encerrar rodada\n3 - Verificar vitória\n\n"))
         if bb.testavitoriabj==True:
             break
-        while pergunta==1:
+        if pergunta==1:
             bb.puxacarta()
             bb.mostramao()
-            pergunta = int(input("\n1 - Puxar mais uma carta\n2 - Encerrar rodada\n3 - Verificar vitória\n\n"))
         if pergunta==2:
             bb.pcjoga()
             bb.mostramao()
-            pergunta = int(input("\n1 - Puxar mais uma carta\n2 - Encerrar rodada\n3 - Verificar vitória\n\n"))
-            while pergunta==1:
-                bb.puxacarta()
-                bb.mostramao()
-                if bb.testavitoriabj() == False:
-                    pergunta = int(input("\n1 - Puxar mais uma carta\n2 - Encerrar rodada\n3 - Verificar vitória\n\n"))
         if pergunta==3:
             if bb.testavitoriapontos():
                 print("Parabéns. Você venceu!")
